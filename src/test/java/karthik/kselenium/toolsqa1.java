e74133c3a60c73c64cd207e02ab4625bd10662dd
Karthik <infotokarthiks@gmail.com>
2016-10-26 19:04:22 +0200

    adding first functional test

---
+++ src/test/java/karthik/kselenium/toolsqa1.java
@@ -0,0 +1,32 @@
+package karthik.kselenium;
+
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+
+public class toolsqa1 {
+	public static void main(String[] args) {
+		/*
+		 System.setProperty("webdriver.chrome.driver",
+		 "C:\\Users\\karthik\\Desktop\\TestLeaf\\Materials\\64 bit\\driver\\chromedriver.exe");
+		 */
+		// System.setProperty("webdriver.chrome.driver","C:\\Users\\karthik\\Desktop\\TestLeaf\\Materials\\64 bit\\driver\\chrome.exe");
+		/* WebDriver driver = new ChromeDriver(); */
+//		MarionetteDriverManager.getInstance().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://store.demoqa.com/");
+		String title = driver.getTitle();
+		String winhandle = driver.getWindowHandle();
+		System.out.println("The Window Handle is :" + winhandle);
+		String contents = driver.getPageSource();
+		System.out.println("The PageContent is :" + contents);
+	}
+
+}
